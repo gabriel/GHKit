3b609474e2349646fbf58f2d2c4e925b6741ed85
Gabriel Handford <gabrielh@gmail.com>
2009-01-25 12:42:56 -0800

    Adding GHLogger

---
+++ Tests/GHLoggerTest.m
@@ -0,0 +1,18 @@
+//
+//  GHLoggerTest.m
+//  GHKit
+//
+//  Created by Gabriel Handford on 1/24/09.
+//  Copyright 2009. All rights reserved.
+//
+
+@interface GHLoggerTest : SenTestCase { }
+@end
+
+@implementation GHLoggerTest
+
+- (void)testLogging {	
+	GHDebug(@"Test debug");
+}
+
+@end
