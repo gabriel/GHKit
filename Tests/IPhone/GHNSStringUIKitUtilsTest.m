a2d5997a6b6192ba9dbf7b4fc441bc4b099e9db9
Gabriel Handford <gabrielh@gmail.com>
2009-03-26 16:12:43 -0700

    Adding test

--- Tests/IPhone/GHNSStringUIKitUtilsTest.m
+++ Tests/IPhone/GHNSStringUIKitUtilsTest.m
@@ -12,7 +12,7 @@
 @implementation GHNSStringUIKitUtilsTest
 
 - (void)testSize {
-	NSArray *lines = nil;
+	NSMutableArray *lines = nil;
 	NSString *text = @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do";
 	[text gh_sizeWithFont:[UIFont systemFontOfSize:16.0] forWidth:100 lineGap:4 lines:&lines maxLineCount:100 truncated:nil options:0];
 	
@@ -20,6 +20,21 @@
 														@"dolor sit",
 														@"amet,",
 														@"consectetur",
+														@"adipisicing",
+														@"elit, sed do", nil];
+	
+	GHAssertTrue([lines isEqualToArray:expectedLines], nil);
+}
+
+- (void)testSizeWithSpaces {
+	NSMutableArray *lines = nil;
+	NSString *text = @"Lorem ipsum dolor sit amet, consectetur   adipisicing elit, sed do";
+	[text gh_sizeWithFont:[UIFont systemFontOfSize:16.0] forWidth:100 lineGap:4 lines:&lines maxLineCount:100 truncated:nil options:0];
+	NSLog(@"lines=%@", lines);
+	NSArray *expectedLines = [NSArray arrayWithObjects:@"Lorem ipsum",
+														@"dolor sit",
+														@"amet,",
+														@"consectetur  ",
 														@"adipisicing",
 														@"elit, sed do", nil];
 	
