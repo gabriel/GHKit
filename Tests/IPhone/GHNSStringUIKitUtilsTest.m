443e524aee706dbf532b0619a53ad1bbe7fd056a
Gabriel Handford <gabrielh@gmail.com>
2009-06-29 15:17:46 -0700

    Removing tests for lame line formatter

--- Tests/IPhone/GHNSStringUIKitUtilsTest.m
+++ Tests/IPhone/GHNSStringUIKitUtilsTest.m
@@ -11,34 +11,5 @@
 
 @implementation GHNSStringUIKitUtilsTest
 
-- (void)testSize {
-	NSMutableArray *lines = nil;
-	NSString *text = @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do";
-	[text gh_sizeWithFont:[UIFont systemFontOfSize:16.0] forWidth:100 lineGap:4 lines:&lines maxLineCount:100 truncated:nil options:0];
-	
-	NSArray *expectedLines = [NSArray arrayWithObjects:@"Lorem ipsum",
-														@"dolor sit",
-														@"amet,",
-														@"consectetur",
-														@"adipisicing",
-														@"elit, sed do", nil];
-	
-	GHAssertTrue([lines isEqualToArray:expectedLines], nil);
-}
-
-- (void)testSizeWithSpaces {
-	NSMutableArray *lines = nil;
-	NSString *text = @"Lorem ipsum dolor sit amet, consectetur   adipisicing elit, sed do";
-	[text gh_sizeWithFont:[UIFont systemFontOfSize:16.0] forWidth:100 lineGap:4 lines:&lines maxLineCount:100 truncated:nil options:0];
-	NSLog(@"lines=%@", lines);
-	NSArray *expectedLines = [NSArray arrayWithObjects:@"Lorem ipsum",
-														@"dolor sit",
-														@"amet,",
-														@"consectetur  ",
-														@"adipisicing",
-														@"elit, sed do", nil];
-	
-	GHAssertTrue([lines isEqualToArray:expectedLines], nil);
-}
 
 @end
